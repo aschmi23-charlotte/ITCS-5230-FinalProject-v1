619d1eaf32b7c003309db4716c834915c836baed
LT-Schmiddy <schmidalex1996@gmail.com>
2026-06-08 15:47:39 -0400

    Custom Event Helper. Untested.

---
+++ Assets/Scripts/Helpers/VisualScriptingCustomEventCaller.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+[RequireComponent(typeof(Variables))]
+public class VisualScriptingCustomEventCaller : MonoBehaviour {
+    [System.Serializable]
+    public class CustomEventCall {
+        public string eventName = "CustomEventName";
+        public string[] argumentVariableNames;
+
+        public void Invoke(GameObject target, Variables vars) {
+            object[] args = new object[argumentVariableNames.Length];
+
+            for (int i = 0; i < args.Length; i++) {
+                args[i] = Variables.Object(vars).Get(argumentVariableNames[i]);
+            }
+
+            EventBus.Trigger("Custom", target, new CustomEventArgs(eventName, args));
+        }
+    }
+    public List<CustomEventCall> customEventCalls;
+
+    public Variables variables { get; private set; }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake() {
+        variables = GetComponent<Variables>();
+    }
+
+    public void TriggerByIndex(int index) {
+        customEventCalls[index].Invoke(gameObject, variables);
+    }
+}
